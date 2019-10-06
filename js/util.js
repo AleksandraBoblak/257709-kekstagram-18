023a2b5a4ba8a17befff98ab139337f5d2185978
Aleksandra Boblak <boblak.aleksandra@gmail.com>
2019-10-06 13:39:22 +0300

    Исправила window.util

--- js/util.js
+++ js/util.js
@@ -5,23 +5,30 @@
   var ESC_KEYCODE = 27;
   var ENTER_KEYCODE = 13;
 
-  window.util = {
-    isEscEvent: function (evt, action) {
-      if (evt.keyCode === ESC_KEYCODE) {
-        action();
-      }
-    },
-    isEnterEvent: function (evt, action) {
-      if (evt.keyCode === ENTER_KEYCODE) {
-        action();
-      }
-    },
-    hide: function (element) {
-      element.classList.add(HIDDEN_CLASS);
-    },
-    show: function (element) {
-      element.classList.remove(HIDDEN_CLASS);
-    },
+  var isEscEvent = function (evt, action) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      action();
+    }
   };
 
+  var isEnterEvent = function (evt, action) {
+    if (evt.keyCode === ENTER_KEYCODE) {
+      action();
+    }
+  };
+
+  var hide = function (element) {
+    element.classList.add(HIDDEN_CLASS);
+  };
+
+  var show = function (element) {
+    element.classList.remove(HIDDEN_CLASS);
+  };
+
+  window.util = {
+    isEscEvent: isEscEvent,
+    isEnterEvent: isEnterEvent,
+    hide: hide,
+    show: show
+  };
 })();
